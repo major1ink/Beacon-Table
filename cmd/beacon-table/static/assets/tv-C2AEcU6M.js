d303b13c02ce747f905adb897eb8cbdf50639bd7
major <valera.moz@yandex.ru>
2026-08-19 12:21:50 +0300

    Исправление бага регулировки громкости звука сцены, пересобран фронт и js

---
+++ cmd/beacon-table/static/assets/tv-C2AEcU6M.js
@@ -0,0 +1,1 @@
+import"./api-WLhJmSR7.js";import{i}from"./index-CSrCysJ1.js";import"./icons-3nongrBp.js";i({canvasId:"scene",role:"tv"});
--- cmd/beacon-table/static/assets/tv-C4ZCP1Ay.js
+++
@@ -1,1 +0,0 @@
-import"./api-WLhJmSR7.js";import{i}from"./index-B3zxZSH4.js";import"./icons-3nongrBp.js";i({canvasId:"scene",role:"tv"});
