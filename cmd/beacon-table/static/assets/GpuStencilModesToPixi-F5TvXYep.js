aac2d37c6cb30b6a2617c0d5d274e55e22f6f70a
major1ink <valera.moz@yandex.ru>
2026-09-25 10:23:18 +0300

    Сборка фронтенда

--- cmd/beacon-table/static/assets/GpuStencilModesToPixi-F5TvXYep.js
+++ cmd/beacon-table/static/assets/GpuStencilModesToPixi-F5TvXYep.js
@@ -1,4 +1,4 @@
-import{w as m,g as l,S as a}from"./dice-fx-gonIme_j.js";const c={name:"local-uniform-bit",vertex:{header:`
+import{w as m,g as l,S as a}from"./dice-fx-BVJ9U5gn.js";const c={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
